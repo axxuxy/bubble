a4b4b0fe1deefd06cdba4ba1b626055ed8f34acd
axxuxy <axxuxy@gmail.com>
2023-12-13 11:54:50 +0800

    Show the entryFileNames info.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 import ts from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
-import {resolve,relative} from "path";
+import { resolve, relative } from "path";
 
 export default defineConfig({
   input: "src/index.ts",
   output: {
     dir: "dist",
     preserveModules: true,
-    entryFileNames(_){
-      return relative(__dirname,resolve(_.name)+".js")
+    entryFileNames(_) {
+      console.log(relative(__dirname, resolve(__dirname, _.name, ".js")));
+      return relative(__dirname, resolve(_.name) + ".js");
     }
   },
   plugins: [
